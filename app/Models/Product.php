6f2d649edf49b0289006cb5a3e24f52f9e619939
tazoakhalaia <darknemiroff@gmail.com>
2023-06-15 23:12:59 +0400

    add relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,8 @@ class Product extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'desc', 'img', 'price', 'category'];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
